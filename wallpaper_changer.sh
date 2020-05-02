48b9eff4b17206181849bb2f43b38682669e62d7
Wan Leung Wong <wanleungwong@gmail.com>
2020-05-02 18:45:27 +0800

    update to use find to search files in subfolder

--- wallpaper_changer.sh
+++ wallpaper_changer.sh
@@ -24,5 +24,5 @@ PID=$(pgrep gnome-session | xargs | cut -d\  -f1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 GSETTINGS_BACKEND=dconf
 
-PIC=$(ls $DIR/* | shuf -n1)
+PIC=$(find $DIR -type f -iname \*.jpg -o -iname \*.png | shuf -n1)
 gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
